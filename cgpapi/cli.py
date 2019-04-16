278fcda36d66ce015bd76fbf3a02375c805011aa
AHTOHuO75 <antkozh@gmail.com>
2019-04-16 05:40:34 +0300

    Addded GetAccountSettings, UpdateAccountSettings 16.04.2019

--- cgpapi/cli.py
+++ cgpapi/cli.py
@@ -31,7 +31,8 @@ class Cli:
         'timeout': 60 * 5 - 5
     }
 
-    def __init__(self, connection_parms):
+    def __init__(self, connection_parms, default_domain=""):
+        self.default_domain = default_domain
         try:
             self.connection_parms['user'] = connection_parms['user']
         except KeyError:
@@ -54,12 +55,12 @@ class Cli:
         self.sock.settimeout(self.connection_parms['timeout'])
         self.sock.connect((self.connection_parms['host'], self.connection_parms['port']))
         result = self.sock.recv(1024)
-        if self.getclicode(result) != self.CLI_CODE['OK']:
+        if self.__getclicode(result) != self.CLI_CODE['OK']:
             print("Connect response code is not OK")
             sys.exit(1)
         self.sessionid = re.findall('(\<.*\@.*\>)', result)
         if self.connection_parms['ssl_transport']:
-            self.check_response(self.send2cli("STLS"), "STLS")
+            self.__check_response(self.__send2cli("STLS"), "STLS")
             self.sock = ssl.wrap_socket(self.sock)
         else:
             pass
@@ -67,18 +68,18 @@ class Cli:
             md5 = hashlib.md5()
             md5.update(self.sessionid[0])
             md5.update(self.connection_parms['password'])
-            self.check_response(self.send2cli("APOP " + self.connection_parms['user'] + " " + md5.hexdigest()), "APOP")
+            self.__check_response(self.__send2cli("APOP " + self.connection_parms['user'] + " " + md5.hexdigest()), "APOP")
         elif self.connection_parms['webuser_login']:
             pass
-            self.check_response(self.send2cli(
+            self.__check_response(self.__send2cli(
                 "AUTH WEBUSER " + self.connection_parms['user'] + " " + self.connection_parms['password']),
                 "AUTH WEBUSER")
         else:
-            if self.send2cli("USER " + self.connection_parms['user'])[0] != self.CLI_CODE['PASSWORD']:
+            if self.__send2cli("USER " + self.connection_parms['user'])[0] != self.CLI_CODE['PASSWORD']:
                 sys.stderr.write("USER response code is not PASSWORD")
                 sys.exit(1)
-            self.check_response(self.send2cli("PASS " + self.connection_parms['password']), "PASS")
-        self.check_response(self.send2cli("INLINE"), "INLINE")
+            self.__check_response(self.__send2cli("PASS " + self.connection_parms['password']), "PASS")
+        self.__check_response(self.__send2cli("INLINE"), "INLINE")
 
     def __del__(self):
         try:
@@ -86,36 +87,65 @@ class Cli:
         except AttributeError:
             pass
 
-    def getclicode(self, response):
+
+
+    def __getclicode(self, response):
         return re.findall('^(\d+)', response)[0]
 
-    def getclimessage(self, response):
+    def __getclimessage(self, response):
         return re.search('^(\d+) (.*)\\r', response).group(2)
 
-    def send2cli(self, command):
+    def __send2cli(self, command):
         try:
             self.sock.sendall(bytes(command + "\r\n"))
         except socket.error:
             sys.stderr.write("send2cli <" + command + "> failed")
             sys.exit()
         result = self.sock.recv(1024)
-        return (self.getclicode(result), result)
+        return (self.__getclicode(result), result)
 
-    def check_response(self, response, module):
+    def __check_response(self, response, module):
         if response[0] != self.CLI_CODE['OK']:
             sys.stderr.write(module + " response code is " + response[0])
             sys.exit(1)
 
-    def check_response_inline(self, response, module):
+    def __check_response_inline(self, response, module):
         if response[0] != self.CLI_CODE['OK_INLINE']:
             sys.stderr.write(module + " response code is " + response[0])
             sys.exit(1)
+
+    def __setdefaultdomain(self,domain_name):
+        if domain_name == "":
+            domain_name = self.default_domain
+        return domain_name
+
+    def __setdefaultdomainaddress(self,domain_name):
+        domain_name = self.__setdefaultdomain(domain_name)
+        if domain_name != "":
+            domain_name = "@" + domain_name
+        return domain_name
+
+    def __parse_response(self, response):
+        tmp = re.search('\{(.*)\}', self.__getclimessage(response)).group(1).split(";")
+        tmp.remove('')
+        result = {}
+        for val in tmp:
+            result[val.split("=")[0]] = val.split("=")[1]
+        return result
+
+    def __convert_param(self, param):
+        result = "{"
+        for value in param.items():
+            result += value[0] + "=" + value[1] + ";"
+        result += "}"
+        return result
 
     ####################################################################
     #    Account commands
     ####################################################################
 
-    def ListDomainObjects(self, domain_name, limit, filter="", what="", cookie=""):
+    def ListDomainObjects(self, limit, domain_name="", filter="", what="", cookie=""):
+        domain_name = self.__setdefaultdomain(domain_name)
         commandline = "ListDomainObjects " + domain_name
         if filter != "":
             commandline += " FILTER " + filter
@@ -124,44 +154,43 @@ class Cli:
             commandline += " " + what
         if cookie != "":
             commandline += " COOKIE " + cookie
-        response = self.send2cli(commandline)
-        self.check_response_inline(response, "ListDomainObjects")
-        tmp = re.search('\((.*)\)', self.getclimessage(response[1])).group(1).split(",")
+        response = self.__send2cli(commandline)
+        self.__check_response_inline(response, "ListDomainObjects")
+        tmp = re.search('\((.*)\)', self.__getclimessage(response[1])).group(1).split(",")
         result = {}
         result["accounts"] = tmp[0]
         result["forwarders"] = tmp[-1]
         result["aliaces"] = tmp[-2]
-        result["Objects"] = re.search('\{(.*)\}', self.getclimessage(response[1])).group(1).split(";")
+        result["Objects"] = re.search('\{(.*)\}', self.__getclimessage(response[1])).group(1).split(";")
         return result
 
-    def ListAccounts(self, domain_name):
+    def ListAccounts(self, domain_name=""):
+        domain_name = self.__setdefaultdomain(domain_name)
         commandline = "ListAccounts " + domain_name
-        response = self.send2cli(commandline)
-        self.check_response_inline(response, "ListAccounts")
-        tmp = re.search('{(.*)}', self.getclimessage(response[1])).group(1).split(";")
+        response = self.__send2cli(commandline)
+        self.__check_response_inline(response, "ListAccounts")
+        tmp = re.search('{(.*)}', self.__getclimessage(response[1])).group(1).split(";")
         tmp.remove('')
-        result = {y[0]: y[1] for y in [x.split("=") for x in tmp]}
+        result = {}
+        for lines in tmp:
+            result[lines.split("=")[0]] = lines.split("=")[1]
         return result
 
-    def ListDomainTelnums(self, domain_name, limit, filter=""):
+    def ListDomainTelnums(self, limit, domain_name="", filter=""):
+        domain_name = self.__setdefaultdomain(domain_name)
         commandline = "ListDomainTelnums " + domain_name
         if filter != "":
             commandline += " FILTER " + filter
         commandline += " " + str(limit)
-        response = self.send2cli(commandline)
-        self.check_response_inline(response, "ListDomainTelnums")
-        tmp = re.search('{(.*)}', self.getclimessage(response[1])).group(1).split(";")
-        tmp.remove('')
-        result = {}
-        result["count"] = (tmp.pop(0).split("=")[1])
-        result.update({y[0]: y[1] for y in [x.split("=") for x in tmp]})
-        return result
+        response = self.__send2cli(commandline)
+        self.__check_response_inline(response, "ListDomainTelnums")
+        return self.__parse_response(response[1])
 
-    def CreateAccount(self, account_name, account_domain="", account_settings={}, account_type="", account_storage="",
+    def CreateAccount(self, account_name, domain_name="", account_settings={}, account_type="", account_storage="",
                       legacy=False):
+        domain_name = self.__setdefaultdomainaddress(domain_name)
         commandline = "CreateAccount " + account_name
-        if account_domain != "":
-            commandline += '@' + account_domain
+        commandline += domain_name
         if account_type != "":
             commandline += ' ' + account_type
         if account_storage != '':
@@ -169,6 +198,45 @@ class Cli:
         if legacy:
             commandline += ' LEGACY'
         commandline += ' {' + ";".join(["%s=\"%s\"" % (k, v) for k, v in account_settings.items()]) + ';}'
-        response = self.send2cli(commandline)
-        self.check_response(response, "CreateAccount")
+        response = self.__send2cli(commandline)
+        self.__check_response(response, "CreateAccount")
         return True
+
+    def RenameAccount(self, old_account_name, new_account_name, domain_name=""):
+        domain_name = self.__setdefaultdomainaddress(domain_name)
+        commandline = "RenameAccount " + old_account_name + domain_name + " into " + new_account_name + domain_name
+        response = self.__send2cli(commandline)
+        self.__check_response(response, "CreateAccount")
+        return True
+
+    def DeleteAccount(self, account_name, domain_name=""):
+        domain_name = self.__setdefaultdomainaddress(domain_name)
+        commandline = "DeleteAccount " + account_name + domain_name
+        response = self.__send2cli(commandline)
+        self.__check_response(response, "DeleteAccount")
+        return True
+
+    def SetAccountType(self, account_name, account_type, domain_name=""):
+        if domain_name == "":
+            domain_name = self.default_domain
+        if domain_name != "":
+            domain_name = "@" + domain_name
+        commandline = "SetAccountType " + account_name + domain_name + account_type
+        response = self.__send2cli(commandline)
+        self.__check_response(response, "SetAccountType")
+        return True
+
+    def GetAccountSettings(self, account_name, domain_name=""):
+        domain_name = self.__setdefaultdomainaddress(domain_name)
+        commandline = "GetAccountSettings " + account_name + domain_name
+        response = self.__send2cli(commandline)
+        self.__check_response_inline(response, "GetAccountSettings")
+        return self.__parse_response(response[1])
+
+    def UpdateAccountSettings(self, account_name, new_settings,domain_name=""):
+        domain_name = self.__setdefaultdomainaddress(domain_name)
+        commandline = "UpdateAccountSettings " + account_name + domain_name + " " + self.__convert_param(new_settings)
+        response = self.__send2cli(commandline)
+        self.__check_response(response, "UpdateAccountSettings")
+        return True
+
--- test.py
+++ test.py
@@ -2,21 +2,31 @@
 # -*- coding: UTF-8 -*-
 
 import cgpapi
+from pprint import pprint
 
 def main():
     conn_parm = {
         'user': 'postmaster',
-        'password': '123',
+        'password': 'password',
         'ssl_transport': True,
         'secure_login': True,
         'webuser_login': False
     }
 
-    cli = cgpapi.Cli(conn_parm)
-    #cli.ListDomainObjects("test.esrr.rzd", 20, "", "ACCOUNTS ALIASES FORWARDERS")
-    #cli.ListAccounts("test.esrr.rzd")
-    #cli.ListDomainTelnums("test.esrr.rzd", 20, "")
-    cli.CreateAccount("test_from_python", "testmail.esrr.rzd", account_storage="store01",account_type="SlicedMailbox", account_settings={"RealName": "It is a Real Name", "MaxAccountSize": "100K"})
+    cli = cgpapi.Cli(conn_parm,"krwtest.rzd")
+    #pprint(cli.ListDomainObjects(20, "krwtest.rzd", what="ACCOUNTS ALIASES FORWARDERS"))
+    #pprint(cli.ListAccounts())
+    #pprint(cli.ListDomainTelnums( 20))
+    #cli.CreateAccount("test_from_python", "krwtest.rzd", account_type="SlicedMailbox", account_settings={"RealName": "It is a Real Name", "MaxAccountSize": "100K"})
+    #cli.RenameAccount("test_from_python","test_from_python1")
+    #cli.DeleteAccount("test_from_python1")
+    #pprint(cli.GetAccountSettings("test-kozh"))
+    new_settings = {
+        'MaxAccountSize': '100K',
+        'RealName': '"Real Name from UPDATE PYTHON"',
+        'ServiceClass': 'MAIL',
+    }
+    cli.UpdateAccountSettings("test-kozh",new_settings)
 
 if __name__ == '__main__':
     main()
